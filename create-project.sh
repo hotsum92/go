795e6f92b32e739a72d7b55cd54f68e6351bfb5c
hotsuma kawashima <hkawashimajob1992@gmail.com>
2024-03-22 16:57:12 +0900

    update create-project

--- create-project.sh
+++ create-project.sh
@@ -5,5 +5,8 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+mkdir "$1"
+cd "$1"
+
 docker run --rm -it -v $PWD:/go -v /etc/passwd:/etc/passwd:ro -u $(id -u):$(id -g) golang:1.21 go mod init $1
 rm -rf pkg
